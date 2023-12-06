569e0bdd138c99766e5470c5461aeae803509142
ach-Ilhamf <111725086+ach-Ilhamf@users.noreply.github.com>
2023-12-06 10:54:13 +0700

    Update pegawai.py

--- pegawai.py
+++ pegawai.py
@@ -1,10 +1,12 @@
 from tkinter import *
-from PIL import Image, ImageTk
 from tkinter import ttk, messagebox
 from tkinter.simpledialog import askstring
 import sqlite3
-from datetime import datetime
-from config import *
+
+# Fungsi getAdminPassword
+def getAdminPassword():
+    # Gantilah dengan implementasi yang sesuai
+    return "admin"
 
 class PegawaiClass:
     def __init__(self, root):
@@ -14,38 +16,41 @@ class PegawaiClass:
         self.root.config(bg="white")
         self.root.focus_force()
 
-        #ANCHOR - ALL VARIABLES
+        # ANCHOR - ALL VARIABLES
         self.var_searchby = StringVar()
         self.var_searchtxt = StringVar()
 
-
-
-        #ANCHOR - SEARCH BAR
+        # ANCHOR - SEARCH BAR
         searchFrame = LabelFrame(self.root, text="Search", bg="white")
         searchFrame.place(x=50, y=40, width=600, height=60)
 
-        #ANCHOR - OPTIONS
-        cmb_search=ttk.Combobox(searchFrame, textvariable=self.var_searchby,values=("Parking ID", "Plat Nomer"), state='readonly', justify=CENTER)
+        # ANCHOR - OPTIONS
+        cmb_search = ttk.Combobox(searchFrame, textvariable=self.var_searchby, values=("Parking ID", "Plat Nomer"),
+                                  state='readonly', justify=CENTER)
         cmb_search.place(x=10, y=10, width=180)
         cmb_search.current(0)
 
-        txt_search = Entry(searchFrame, textvariable=self.var_searchtxt,bg='yellow').place(x=200, y=10, width=200)
-        btn_search = Button(searchFrame, command=self.search,text="Search", bg='#4caf50', fg='white', cursor='hand2').place(x=410, y=10, width=150, height=20)
+        txt_search = Entry(searchFrame, textvariable=self.var_searchtxt, bg='yellow')
+        txt_search.place(x=200, y=10, width=200)
+        btn_search = Button(searchFrame, command=self.search, text="Search", bg='#4caf50', fg='white', cursor='hand2')
+        btn_search.place(x=410, y=10, width=150, height=20)
 
-        #ANCHOR - TITLE
-        title = Label(self.root, text="Pegawai", font=("goudy old style", 15), bg="#0f4d7d", fg="white").place(x=0, y=0, relwidth=1)
+        # ANCHOR - TITLE
+        title = Label(self.root, text="Pegawai", font=("goudy old style", 15), bg="#0f4d7d", fg="white").place(x=0, y=0,
+                                                                                                            relwidth=1)
 
-        #ANCHOR - EMPLOYEE DETAILS / RESULT
+        # ANCHOR - EMPLOYEE DETAILS / RESULT
         park_frame = Frame(self.root, bd=3, relief=RIDGE)
         park_frame.place(x=-1, y=150, relwidth=1, height=350)
 
         scrolly = Scrollbar(park_frame, orient=VERTICAL)
         scrollx = Scrollbar(park_frame, orient=HORIZONTAL)
 
-        self.PegawaiTable = ttk.Treeview(park_frame, columns=("Pegawai ID", "Nama", "Aktif", "Lihat", "Hapus"), show="headings", yscrollcommand=scrolly.set, xscrollcommand=scrollx.set)
+        self.PegawaiTable = ttk.Treeview(park_frame, columns=("Pegawai ID", "Nama", "Aktif", "Lihat", "Hapus", "Edit"),
+                                         show="headings", yscrollcommand=scrolly.set, xscrollcommand=scrollx.set)
 
-        scrollx.pack(side=BOTTOM,fill=X)
-        scrolly.pack(side=RIGHT,fill=Y)
+        scrollx.pack(side=BOTTOM, fill=X)
+        scrolly.pack(side=RIGHT, fill=Y)
         scrollx.config(command=self.PegawaiTable.xview)
         scrolly.config(command=self.PegawaiTable.yview)
 
@@ -54,21 +59,26 @@ class PegawaiClass:
         self.PegawaiTable.heading("Aktif", text="Aktif")
         self.PegawaiTable.heading("Lihat", text="Lihat")
         self.PegawaiTable.heading("Hapus", text="Hapus")
-
+        self.PegawaiTable.heading("Edit", text="Edit")
 
         self.PegawaiTable.column("Pegawai ID", width=10)
         self.PegawaiTable.column("Nama", width=70)
         self.PegawaiTable.column("Aktif", width=110)
         self.PegawaiTable.column("Lihat", width=110)
         self.PegawaiTable.column("Hapus", width=80)
+        self.PegawaiTable.column("Edit", width=80)
 
         self.PegawaiTable.pack(fill=BOTH, expand=1)
-        # self.PegawaiTable.bind("<ButtonRelease-1>", self.get_data)
-        self.PegawaiTable.bind("<ButtonRelease-1>", lambda event: self.on_pegawai_click(event, self.PegawaiTable, self.detail_pegawai, self.get_password_delete))
+        self.PegawaiTable.bind("<ButtonRelease-1>", lambda event: self.on_pegawai_click(event))
+
+        # Tombol Tambah Pegawai
+        btn_tambah = Button(self.root, command=self.tambah_pegawai, text="Tambah Pegawai", bg='#2196f3', fg='white',
+                            cursor='hand2')
+        btn_tambah.place(x=180, y=110, width=130, height=30, anchor="ne")
 
         self.show()
 
-        self.root.after(1000, self.update_time())
+        self.root.after(1000, self.update_time)
 
     def show(self):
         con = sqlite3.connect(database="parking.db")
@@ -79,13 +89,14 @@ class PegawaiClass:
             self.PegawaiTable.delete(*self.PegawaiTable.get_children())
             for employee_data in rows:
                 pegawai_id, nama, _, aktif = employee_data
-                self.PegawaiTable.insert("", "end", values=(pegawai_id, nama, aktif, "Lihat", "Delete"))
+                self.PegawaiTable.insert("", "end", values=(pegawai_id, nama, aktif, "Lihat", "Delete", "Edit"))
         except Exception as ex:
             messagebox.showerror("Error", f"Error due to : {str(ex)}", parent=self.root)
 
-    #!SECTION FITUR
+    # !SECTION FITUR
     def get_password_delete(self, pegawai_id):
-        password_inp = askstring("Password Prompt", "Hanya admin yang dapat menghapus\nEnter your password:", parent=self.root, show="*")
+        password_inp = askstring("Password Prompt",
+                                 "Hanya admin yang dapat menghapus\nEnter your password:", parent=self.root, show="*")
         if password_inp == getAdminPassword():
             print("Entered Correct:", password_inp)
             self.delete(pegawai_id)
@@ -104,10 +115,14 @@ class PegawaiClass:
                 messagebox.showinfo("Delete", "Employee Deleted Successfully", parent=self.root)
         except Exception as ex:
             messagebox.showerror("Error", f"Error due to : {str(ex)}")
-    
+
+    def edit_pegawai(self, pegawai_id):
+        self.new_win_edit = Toplevel(self.root)
+        self.new_obj_edit = EditPegawaiClass(self.new_win_edit, pegawai_id, self.show)
+
 
     def search(self):
-        con = sqlite3.connect(database = "parking.db")
+        con = sqlite3.connect(database="parking.db")
         cur = con.cursor()
         print(self.var_searchtxt.get())
         print(self.var_searchby.get())
@@ -121,13 +136,14 @@ class PegawaiClass:
                 elif self.var_searchby.get() == "Plat Nomer":
                     self.var_searchby.set("plat")
 
-                cur.execute("select * from parking where "+self.var_searchby.get()+ " LIKE '%"+self.var_searchtxt.get()+"%'")
+                cur.execute(
+                    "select * from pegawai where " + self.var_searchby.get() + " LIKE '%" + self.var_searchtxt.get() + "%'")
                 rows = cur.fetchall()
                 if len(rows) != 0:
-                    self.PegawaiTable.delete(*self.ParkingTable.get_children())
+                    self.PegawaiTable.delete(*self.PegawaiTable.get_children())
                     rows = [(t[0], t[1], t[2], t[5], t[3], t[4], t[6]) for t in rows]
                     for row in rows:
-                        self.ParkingTable.insert('', END, values=row)
+                        self.PegawaiTable.insert('', END, values=row)
                 else:
                     messagebox.showerror("Error", "No record found", parent=self.root)
 
@@ -136,35 +152,39 @@ class PegawaiClass:
 
     def detail_pegawai(self, pegawai_id):
         self.new_win = Toplevel(self.root)
-        self.new_obj=DetailPegawaiClass(self.new_win, pegawai_id)
+        self.new_obj = DetailPegawaiClass(self.new_win, pegawai_id, self.show)
 
+    def tambah_pegawai(self):
+        self.new_win_tambah = Toplevel(self.root)
+        self.new_obj_tambah = TambahPegawaiClass(self.new_win_tambah, self.show)
 
-    def on_pegawai_click(self, event, tree, open_func, delete_func):
-    # Function to handle button clicks in the treeview
-        item_id = tree.identify_row(event.y)
+    def on_pegawai_click(self, event):
+        item_id = self.PegawaiTable.identify_row(event.y)
         if item_id:
-            item = tree.item(item_id)
-            column = tree.identify_column(event.x)
-            col_name = tree.heading(column)["text"]
+            item = self.PegawaiTable.item(item_id)
+            column = self.PegawaiTable.identify_column(event.x)
+            col_name = self.PegawaiTable.heading(column)["text"]
 
             if col_name == "Lihat":
                 pegawai_id = item["values"][0]
                 print(f'Lihat : {pegawai_id}')
-                open_func(pegawai_id)
+                self.detail_pegawai(pegawai_id)
             elif col_name == "Hapus":
                 pegawai_id = item["values"][0]
                 print(pegawai_id)
-                delete_func(pegawai_id)
-
+                self.get_password_delete(pegawai_id)
+            elif col_name == "Edit":
+                pegawai_id = item["values"][0]
+                print(f'Edit : {pegawai_id}')
+                self.edit_pegawai(pegawai_id)
 
     def update_time(self):
         self.show()
         self.root.after(1000, self.update_time)
 
 
-class DetailPegawaiClass (PegawaiClass):
-    def __init__(self, root, pegawai_id):
-        # super().__init__(root)
+class DetailPegawaiClass(PegawaiClass):
+    def __init__(self, root, pegawai_id, refresh_func):
         self.root = root
         self.root.geometry("700x500+400+150")
         self.root.title("Parking Management System | Tambah Pegawai")
@@ -175,9 +195,124 @@ class DetailPegawaiClass (PegawaiClass):
         lbl_plat = Label(self.root, text=self.pegawai_id, font=("goudy old style", 12), bg="white").place(x=350, y=120)
         print(f'pegawai id nya nih {pegawai_id}')
 
-        title = Label(self.root, text="Checkout Parking", font=("goudy old style", 15), bg="#0f4d7d", fg="white").place(x=0, y=0, relwidth=1)
-    
-if __name__== "__main__":
+        title = Label(self.root, text="Checkout Parking", font=("goudy old style", 15), bg="#0f4d7d", fg="white").place(
+            x=0, y=0, relwidth=1)
+
+        # Tombol Edit Pegawai
+        btn_edit = Button(self.root, command=lambda: self.edit_pegawai(pegawai_id), text="Edit Pegawai", bg='#ff9800',
+                          fg='white', cursor='hand2')
+        btn_edit.place(x=350, y=250, width=130, height=25)
+
+        # Tombol Hapus Pegawai
+        btn_hapus = Button(self.root, command=lambda: self.get_password_delete(pegawai_id), text="Hapus Pegawai",
+                           bg='#f44336', fg='white', cursor='hand2')
+        btn_hapus.place(x=500, y=250, width=130, height=25)
+
+    def edit_pegawai(self, pegawai_id):
+        # Perbaikan: implementasikan fungsi untuk mengedit pegawai di sini
+        print(f'Edit Pegawai : {pegawai_id}')
+
+class EditPegawaiClass:
+    def __init__(self, root, pegawai_id, refresh_func):
+        self.root = root
+        self.root.geometry("400x300+500+200")
+        self.root.title("Edit Pegawai")
+        self.root.config(bg="white")
+        self.root.focus_force()
+
+        self.refresh_func = refresh_func
+        self.pegawai_id = pegawai_id
+
+        # Variables
+        self.var_nama = StringVar()
+        self.var_password = StringVar()
+
+        # Fetch data for the selected employee
+        self.fetch_data()
+
+        # Label and Entry for Name
+        lbl_nama = Label(self.root, text="Nama Pegawai:", font=("goudy old style", 12), bg="white").place(x=50, y=50)
+        txt_nama = Entry(self.root, textvariable=self.var_nama, font=("goudy old style", 12), bg='lightgray').place(x=200, y=50)
+
+        # Label and Entry for Password
+        lbl_password = Label(self.root, text="Password:", font=("goudy old style", 12), bg="white").place(x=50, y=100)
+        txt_password = Entry(self.root, textvariable=self.var_password, show="*", font=("goudy old style", 12), bg='lightgray').place(x=200, y=100)
+
+        # Button to Update
+        btn_simpan = Button(self.root, command=self.update_pegawai, text="Update", bg='#4caf50', fg='white', cursor='hand2')
+        btn_simpan.place(x=150, y=200, width=100, height=25)
+
+    def fetch_data(self):
+        try:
+            con = sqlite3.connect(database="parking.db")
+            cur = con.cursor()
+            cur.execute("SELECT nama_pegawai, password FROM pegawai WHERE pegawai_id=?", (self.pegawai_id,))
+            row = cur.fetchone()
+            if row:
+                self.var_nama.set(row[0])
+                self.var_password.set(row[1])
+            con.close()
+        except Exception as ex:
+            messagebox.showerror("Error", f"Error: {str(ex)}", parent=self.root)
+
+    def update_pegawai(self):
+        try:
+            con = sqlite3.connect(database="parking.db")
+            cur = con.cursor()
+            cur.execute("UPDATE pegawai SET nama_pegawai=?, password=? WHERE pegawai_id=?",
+                        (self.var_nama.get(), self.var_password.get(), self.pegawai_id))
+            con.commit()
+            con.close()
+            messagebox.showinfo("Success", "Pegawai Updated Successfully", parent=self.root)
+            self.refresh_func()
+            self.root.destroy()
+        except Exception as ex:
+            messagebox.showerror("Error", f"Error: {str(ex)}", parent=self.root)
+
+class TambahPegawaiClass:
+    def __init__(self, root, refresh_func):
+        self.root = root
+        self.root.geometry("400x300+500+200")
+        self.root.title("Tambah Pegawai")
+        self.root.config(bg="white")
+        self.root.focus_force()
+
+        self.refresh_func = refresh_func
+
+        # Variable
+        self.var_nama = StringVar()
+        self.var_password = StringVar()
+
+        # Label dan Entry
+        lbl_nama = Label(self.root, text="Nama Pegawai:", font=("goudy old style", 12), bg="white").place(x=50, y=50)
+        txt_nama = Entry(self.root, textvariable=self.var_nama, font=("goudy old style", 12), bg='lightgray').place(
+            x=200, y=50)
+
+        lbl_password = Label(self.root, text="Password:", font=("goudy old style", 12), bg="white").place(x=50, y=100)
+        txt_password = Entry(self.root, textvariable=self.var_password, show="*", font=("goudy old style", 12),
+                             bg='lightgray').place(x=200, y=100)
+
+        # Tombol Simpan
+        btn_simpan = Button(self.root, command=self.simpan_pegawai, text="Simpan", bg='#4caf50', fg='white',
+                            cursor='hand2')
+        btn_simpan.place(x=150, y=250, width=100, height=25)
+
+    def simpan_pegawai(self):
+        try:
+            con = sqlite3.connect(database="parking.db")
+            cur = con.cursor()
+            cur.execute("INSERT INTO pegawai (nama_pegawai, password, aktif) VALUES (?, ?, ?)",
+                        (self.var_nama.get(), self.var_password.get(), "Aktif"))
+            con.commit()
+            con.close()
+            messagebox.showinfo("Sukses", "Pegawai Ditambahkan", parent=self.root)
+            self.refresh_func()
+            self.root.destroy()
+        except Exception as ex:
+            messagebox.showerror("Error", f"Error: {str(ex)}", parent=self.root)
+
+
+if __name__ == "__main__":
     root = Tk()
     obj = PegawaiClass(root)
     root.mainloop()
